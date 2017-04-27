efa93c14b633959681432e1430a699f97c33bc22
Maxime Rainville <maxime@syrp.co.nz>
2017-04-27 13:00:07 +1200

    Initial commit.

---
+++ src/sample.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sample-component',
+  templateUrl: './sample.component.html',
+})
+export class SampleComponent {
+
+  constructor() {
+  }
+
+}
