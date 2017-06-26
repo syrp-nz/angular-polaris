db65b09c0422623aa84da9159569f722641775a0
Maxime Rainville <maxime@syrp.co.nz>
2017-06-26 16:14:24 +1200

    Add some test logic for Showing the Wysiwig.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'app works!';
+    simpleTextField = "Bonjour monde";
 
     complexToolbar = [
         ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
