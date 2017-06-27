baac70b92df425b97e69b1d4692569bc0a89ffcb
Maxime Rainville <maxime@syrp.co.nz>
2017-06-27 17:04:23 +1200

    Complete partial implementation of pages.

--- src/app/library/page/page.header.component.ts
+++ src/app/library/page/page.header.component.ts
@@ -6,15 +6,16 @@ import { AngularComplexAction } from '../types';
  * Component to display a Shopify layout
  */
 @Component({
-    selector: 'plrsPage',
-    templateUrl: './page.component.html',
+    selector: 'plrsPageHeader',
+    templateUrl: './page.header.component.html',
     host: {
-        '[class.Polaris-Page]': 'true',
-        '[class.Polaris-Page--fullWidth]': 'fullWidth !== false'
+        '[class.Polaris-Page__Header]': 'true',
+        '[class.Polaris-Page__Header--hasBreadcrumbs]': 'breadcrumbs !== undefined',
+        '[class.Polaris-Page__Header--hasPagination]': 'pagination !== undefined',
     },
-    styles: [':host {display: block;}']
+    styles: [':host {display: block;}'],
 })
-export class PageComponent implements OnInit {
+export class PageHeaderComponent implements OnInit {
 
 
     @Input() public title: string = '';
@@ -23,11 +24,11 @@ export class PageComponent implements OnInit {
     @Input() public secondaryActions: AngularComplexAction[];
     @Input() public primaryAction: AngularComplexAction;
     @Input() public pagination: PaginationDescriptor;
-    @Input() public fullWidth: boolean = false;
 
     /**
      * Automatically add a sectionned to the layout.
      */
     ngOnInit() {
+
     }
 }
