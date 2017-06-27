313bbb07594c68ef2c92369b89b092ec58346fe8
Maxime Rainville <maxime@syrp.co.nz>
2017-06-27 17:07:08 +1200

    Add a reference to the ng router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 import { AngularPolarisModule } from './library'
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
@@ -15,6 +18,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule,
     AngularPolarisModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
