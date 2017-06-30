8a354ef5c64a30da071297650b9e5481e082501a
Maxime Rainville <maxime@syrp.co.nz>
2017-06-30 17:17:32 +1200

    Handle the dismissible banner.

--- src/app/doc/components/banner.component.ts
+++ src/app/doc/components/banner.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, ViewChild} from '@angular/core';
 import { DocService } from '../doc.service';
 import { PolarisComponent } from '../doc.data';
 import { ComponentComponent } from '../utilities/component.component';
+import * as Polaris from '../../library';
 
 @Component({
     templateUrl: 'banner.component.html'
@@ -10,11 +11,17 @@ export class BannerComponent extends ComponentComponent {
 
     protected componentPath: string = 'banner/banner.component';
 
+    @ViewChild('banner') private banner: Polaris.Banner.BannerComponent;
+
+    private subscription: any = false;
+
+
     content:string = 'Hello world';
     title:string = '';
     icon:string = '';
     status:string|false = '';
     progress:string|false = '';
+    logDismiss: boolean = false;
 
     statusOptions = ['', 'default', 'success', 'info', 'warning', 'critical'];
 
@@ -24,13 +31,21 @@ export class BannerComponent extends ComponentComponent {
         super(service);
     }
 
+    dismissUpdate() {
+        if (this.logDismiss) {
+            this.subscription = this.banner.dismiss.subscribe((event) => {this.eventLog(event, 'dismiss')});
+        } else {
+            this.subscription.unsubscribe();
+        }
+    }
+
     public get code(): string {
         const status = this.nullableAttr('status');
-        const progress = this.nullableAttr('progress');
+        const dismiss = this.eventAttr('dismiss');
 
-return `<plrsBadge${status}${progress}>
+return `<plrsBanner${status}${dismiss}>
     ${this.content}
-</plrsBadge>`;
+</plrsBanner>`;
     }
 
 }
