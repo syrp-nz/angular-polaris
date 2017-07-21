d4fe27c0a0801fb965599b2994da29253331832b
Maxime Rainville <maxime@syrp.co.nz>
2017-07-21 12:47:27 +1200

    Add syntax highlighting to the code card component.

--- src/app/doc/utilities/code.card.component.ts
+++ src/app/doc/utilities/code.card.component.ts
@@ -1,8 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import * as Prism from 'prismjs';
+
+
+
 @Component({
     selector: 'codeCard',
     template: `<plrsCard sectioned [title]="title">
-        <pre><code>{{ code }}</code></pre>
+        <pre><code #codeBlock class="language-{{language}}">{{ code }}</code></pre>
     </plrsCard>`,
     styles: [
         "plrscard {margin-top: 2rem;}",
@@ -10,6 +14,33 @@ import { Component, Input } from '@angular/core';
     ]
 })
 export class CodeCardComponent {
-    @Input() public code = '';
+
+    _code = ''
+
+    @Input()
+    public get code(): string {
+        return this._code;
+    };
+
+    public set code(value: string) {
+        console.dir(value);
+        this._code = value;
+        this.highlight();
+    }
+
+    @Input() public language: string = "html";
+
+
     @Input() public title = 'Code';
+
+    @ViewChild('codeBlock') codeBlock: ElementRef;
+
+    ngOnInit() {
+        Prism.highlightElement(this.codeBlock.nativeElement, false);
+        this.highlight();
+    }
+
+    private highlight() {
+        this.codeBlock.nativeElement.innerHTML = Prism.highlight(this.code, Prism.languages[this.language]);
+    }
 }
